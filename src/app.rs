33539f91d8089188447f8e38e0851c2b2a06e413
Franz Chuquirachi <franz.chuquirachi@gmail.com>
2025-02-26 02:33:41 +0900

    Playing with viewport modifiers. Name changed to eframe_test

--- src/app.rs
+++ src/app.rs
@@ -46,51 +46,55 @@ impl eframe::App for TemplateApp {
         // Put your widgets into a `SidePanel`, `TopBottomPanel`, `CentralPanel`, `Window` or `Area`.
         // For inspiration and more examples, go to https://emilk.github.io/egui
 
-        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            // The top panel is often a good place for a menu bar:
+        // egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
+        //     // The top panel is often a good place for a menu bar:
 
-            egui::menu::bar(ui, |ui| {
-                // NOTE: no File->Quit on web pages!
-                let is_web = cfg!(target_arch = "wasm32");
-                if !is_web {
-                    ui.menu_button("File", |ui| {
-                        if ui.button("Quit").clicked() {
-                            ctx.send_viewport_cmd(egui::ViewportCommand::Close);
-                        }
-                    });
-                    ui.add_space(16.0);
-                }
+        //     egui::menu::bar(ui, |ui| {
+        //         // NOTE: no File->Quit on web pages!
+        //         let is_web = cfg!(target_arch = "wasm32");
+        //         if !is_web {
+        //             ui.menu_button("File", |ui| {
+        //                 if ui.button("Quit").clicked() {
+        //                     ctx.send_viewport_cmd(egui::ViewportCommand::Close);
+        //                 }
+        //             });
+        //             ui.add_space(16.0);
+        //         }
 
-                egui::widgets::global_theme_preference_buttons(ui);
-            });
-        });
+        //         egui::widgets::global_theme_preference_buttons(ui);
+        //     });
+        // });
 
-        egui::CentralPanel::default().show(ctx, |ui| {
-            // The central panel the region left after adding TopPanel's and SidePanel's
-            ui.heading("eframe template");
+        // egui::CentralPanel::default().show(ctx, |ui| {
+        //     // The central panel the region left after adding TopPanel's and SidePanel's
+        //     ui.heading("eframe template");
 
-            ui.horizontal(|ui| {
-                ui.label("Write something: ");
-                ui.text_edit_singleline(&mut self.label);
-            });
+        //     ui.horizontal(|ui| {
+        //         ui.label("Write something: ");
+        //         ui.text_edit_singleline(&mut self.label);
+        //     });
 
-            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                self.value += 1.0;
-            }
+        //     ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
+        //     if ui.button("Increment").clicked() {
+        //         self.value += 1.0;
+        //     }
 
-            ui.separator();
+        //     ui.separator();
 
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/main/",
-                "Source code."
-            ));
+        //     ui.add(egui::github_link_file!(
+        //         "https://github.com/emilk/eframe_template/blob/main/",
+        //         "Source code."
+        //     ));
 
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                powered_by_egui_and_eframe(ui);
-                egui::warn_if_debug_build(ui);
-            });
-        });
+        //     ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
+        //         powered_by_egui_and_eframe(ui);
+        //         egui::warn_if_debug_build(ui);
+        //     });
+        // });
+    }
+
+    fn clear_color(&self, _visuals: &egui::Visuals) -> [f32; 4] {
+        _visuals.window_fill().to_normalized_gamma_f32()
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -8,19 +8,35 @@ fn main() -> eframe::Result {
 
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-            .with_inner_size([400.0, 300.0])
-            .with_min_inner_size([300.0, 220.0])
-            .with_icon(
-                // NOTE: Adding an icon is optional
-                eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon-256.png")[..])
-                    .expect("Failed to load icon"),
-            ),
+            .with_inner_size([600.0, 600.0])
+            // .with_min_inner_size([300.0, 220.0])
+            // .with_icon(
+            //     // NOTE: Adding an icon is optional
+            //     eframe::icon_data::from_png_bytes(&include_bytes!("../assets/icon-256.png")[..])
+            //         .expect("Failed to load icon"),
+            // )
+            .with_close_button(false)
+            .with_title("")
+            .with_maximize_button(false)
+            .with_resizable(true)
+            .with_minimize_button(false)
+            // .with_always_on_top()
+            .with_titlebar_buttons_shown(false)
+            // .with_mouse_passthrough(true)
+            .with_active(true)
+            .with_transparent(true)
+            ,
         ..Default::default()
     };
     eframe::run_native(
         "eframe template",
         native_options,
-        Box::new(|cc| Ok(Box::new(eframe_template::TemplateApp::new(cc)))),
+        Box::new(
+          |cc| 
+          // cc.raw_window_handle = windowHandler.raw_window_handle().unwrap();
+          Ok(Box::new(eframe_test::TemplateApp::new(cc)))
+          ),
+          
     )
 }
 
@@ -50,7 +66,7 @@ fn main() {
             .start(
                 canvas,
                 web_options,
-                Box::new(|cc| Ok(Box::new(eframe_template::TemplateApp::new(cc)))),
+                Box::new(|cc| Ok(Box::new(eframe_test::TemplateApp::new(cc)))),
             )
             .await;
 
